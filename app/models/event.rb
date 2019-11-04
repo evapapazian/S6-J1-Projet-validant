fa6446004ab67ee4071130e6e6d705496c34b814
evapapazian <eva.papazian@ieseg.fr>
2019-11-04 15:25:06 +0100

    la bdd marche amen

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
-	validates :start_date, presence: true , no_past_event: {message: "la date de départ ne peut pas être dans le passé"}
-	validates :duration, presence: true, multiple_of_5: {message: "la durée doit être un multiple de 5"}
-	validates :title, presence: true , length: { minimum: 5 }, length: { maximum: 140}
-	validates :description, presence: true, length: {minimum: 20}, length: { maximum: 1000}
+	validates :start_date, presence: true 
+	validate  :no_past_event
+	validates :duration, presence: true
+	validate  :multiple_of_5
+	validates :title, presence: true , length: { minimum: 5 , maximum: 140}
+	validates :description, presence: true, length: {minimum: 20, maximum: 1000}
 	validates :price, presence: true, numericality: {greater_than: 0, less_than: 1001}
 	validates :location, presence: true 
 
@@ -10,9 +12,9 @@ class Event < ApplicationRecord
 	
 def multiple_of_5
 
-    errors.add(:duration, "la durée doit être un multiple de 5") unless
-    self.duration %5 == 0 
-end
+     errors.add(:duration, "La duree doit etre un multiple de 5 !") unless
+    self.duration % 5 == 0
+  end
 
 
 def no_past_event
@@ -23,4 +25,6 @@ end
 
 has_many :attendances
 has_many :users ,through: :attendance 
-belongs_to :user #à supprimer potentiellement, stay tuned 
+
+
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = []
+e = []
+a = []
+chiffre_multiple_5 = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
+
+10.times do |index|
+	u << User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 10), email: Faker::Name.first_name + "@yopmail.com")
+	#puts "User : #{index.first_name}" #me donne le nom de l'utilisateur créé
+
+	e << Event.create(start_date: Faker::Date.forward(days: 365), duration: chiffre_multiple_5.sample, title: Faker::Book.title, description: Faker::Lorem.sentence(word_count: 10), price: rand(1..1000), location: Faker::Address.city)
+end
+
+10.times do |index|
+	a << Attendance.create(user: u.sample, event: e.sample)
+end 
